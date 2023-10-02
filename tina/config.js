9d571d974933b7b440fccd8f369eb3062e96c398
syahrulfadli <callsyahrul@outlook.com>
2023-10-03 00:55:51 +0800

    fix 3rd

---
+++ tina/config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "4c5f4747-2d58-444d-adc8-91586c3b981c", // Get this from tina.io
+  token: "9e5eaa5ea2dec883808d969cfdb65eb267b4b776", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "docs",
+        label: "Docs",
+        path: "content/docs",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
