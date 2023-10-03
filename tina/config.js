980f744b13376d519fa9128efbb254f7958acba0
syahrulfadli <callsyahrul@outlook.com>
2023-10-03 15:21:47 +0800

    add collection

--- tina/config.js
+++ tina/config.js
@@ -40,6 +40,47 @@ export default defineConfig({
           },
         ],
       },
+      {
+        name: "pages",
+        label: "Pages",
+        path: "content",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "blog",
+        label: "Blog",
+        path: "content/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
     ],
   },
   search: {
