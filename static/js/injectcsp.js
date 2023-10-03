c1c250114ebcd1aa8dee60825676ea91fcbd58ef
syahrulfadli <callsyahrul@outlook.com>
2023-10-03 14:26:30 +0800

    098

--- static/js/injectcsp.js
+++ static/js/injectcsp.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function(){
     console.log('injecting');
     const head = document.head;
-    let csp = `<meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline' 'unsafe-eval'">`;
+    let csp = `<meta http-equiv="Content-Security-Policy" content="default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline';">`;
     head.insertAdjacentHTML('afterbegin', csp);
     console.log('injected');
   });
