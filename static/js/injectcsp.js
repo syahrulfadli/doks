82ac7e56cf04a46c8b8637745f8b96f728359cee
syahrulfadli <callsyahrul@outlook.com>
2023-10-03 14:03:00 +0800

    123

--- static/js/injectcsp.js
+++ static/js/injectcsp.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function(){
     console.log('injecting');
     const head = document.head;
-    let csp = `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://content.tinajs.io https://identity.tinajs.io; font-src 'self'; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; report-uri https://651b9ffb0fcafd85d341e570.endpoint.csper.io/?v=2; worker-src 'none';">`;
+    let csp = `<meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline' 'unsafe-eval'">`;
     head.insertAdjacentHTML('afterbegin', csp);
     console.log('injected');
   });
