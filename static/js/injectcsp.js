87d9bcceab6e4411b06cdea9fab17e59e0234e2c
syahrulfadli <callsyahrul@outlook.com>
2023-10-04 08:28:43 +0800

    new CSP rules

--- static/js/injectcsp.js
+++
@@ -1,8 +0,0 @@
-// inject CSP
-window.addEventListener('load', function(){
-    console.log('injecting');
-    const head = document.head;
-    let csp = `<meta http-equiv="Content-Security-Policy" content="default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline';">`;
-    head.insertAdjacentHTML('afterbegin', csp);
-    console.log('injected');
-  });
